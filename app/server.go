9fd5e7ef8337ab9e1b07cdf979f499b098e1acab
ayadav16 <ayadav7@binghamton.edu>
2024-06-07 00:21:52 -0400

    pass stage

--- app/server.go
+++ app/server.go
@@ -115,7 +115,7 @@ func handleConnection(conn net.Conn) {
 				conn.Write([]byte(response))
 			} else {
 				file.Write([]byte(body))
-				response := VERSION + " 200 OK" + CLRF + CLRF
+				response := VERSION + " 201 OK" + CLRF + CLRF
 				conn.Write([]byte(response))
 			}
 		}
